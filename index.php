012c6fd98348e55d22d84f742e3ca92473c86f66
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-12 16:35:13 +0100

    create array of products in a new file

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
 require_once __DIR__ . "/Models/Products.php";
 require_once __DIR__ . "/Models/Category.php";
-
-
-$ciotola = new Products(
-    "Ciotola per gatti",
-    01,
-    "Ciotola per gatti di medie dimensioni",
-    "",
-    new Category("cane"),
-    50
-);
-
-var_dump($ciotola);
